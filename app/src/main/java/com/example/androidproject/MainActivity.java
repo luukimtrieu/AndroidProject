458acced3e1d2e705ce0b49dcdb13ea138621013
H1eu69 <anrayno200@gmail.com>
2022-04-13 15:16:42 +0700

    created bottom menu and some blank fragments

--- app/src/main/java/com/example/androidproject/MainActivity.java
+++ app/src/main/java/com/example/androidproject/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.androidproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import java.sql.Time;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+                switch (item.getItemId())
+                {
+                    case R.id.calendarFragment:
+                        fragment = new CalendarFragment();
+                        break;
+                    case R.id.timelineFragment:
+                        fragment = new TimelineFragment();
+                        break;
+                    case R.id.reportFragment:
+                        fragment = new ReportFragment();
+                        break;
+                    case R.id.settingFragment:
+                        fragment = new SettingFragment();
+                        break;
+
+                }
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.replace(R.id.fragmentContainerView2, fragment).commit();
+                return true;
+            }
+        });
+
+
     }
 }
